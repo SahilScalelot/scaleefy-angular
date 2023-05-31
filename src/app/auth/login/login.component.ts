e8eddbe11cea7932929c2a49f977ffe4e4418160
Sudeep <sudeep.scalelot@gmail.com>
2023-05-31 16:44:58 +0530

    login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.sass']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.loginForm = this._formBuilder.group({
+      email: [''],
+      password: ['']
+    })
+  }
+
+  login(): any {
+    console.log(this.loginForm.value)
+  }
 }
